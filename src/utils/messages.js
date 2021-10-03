97c442888c6241a927e97b91db33adab255642de
snootiecute <dangtiendat288@gmail.com>
2021-10-03 19:04:38 +0700

    first commit

---
+++ src/utils/messages.js
@@ -0,0 +1,17 @@
+const generateMessage = (username, text) => {
+  return {
+    username,
+    text,
+    createdAt: new Date().getTime(),
+  };
+};
+
+const generateLocationMessage = (username, text) => {
+  return {
+    username,
+    url: text,
+    createdAt: new Date().getTime(),
+  };
+};
+
+module.exports = { generateMessage, generateLocationMessage };
